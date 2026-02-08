ae42076cea40ac21470bab94fcf8ac11d18c0ce6
YosefTamer <programmeryosef@gmail.com>
2026-02-08 07:04:46 +0200

    Implement SaveMyName contract with storage functions

---
+++ Day02/SaveMyName.sol
@@ -0,0 +1,28 @@
+/// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract SaveMyName {
+    struct Person{
+        string name;
+        string bio;
+        uint age;
+    }
+    Person[]  person;
+
+    function saveName(
+        string memory _name, 
+        string memory _bio, 
+        uint _age) public {
+
+            person.push(Person(_name, _bio, _age));
+        }
+    
+
+    function getPerson(uint _index) public view returns(string memory, string memory, uint){
+        Person memory _person = person[_index];
+        return(_person.name, _person.bio, _person.age);
+    }
+
+
+
+}
